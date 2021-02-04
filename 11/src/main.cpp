2f769aa79de8728bbd1498f29f81485ab6b44af2
dazzlemon <daanill13470@gmail.com>
2021-02-04 17:46:58 +0200

    added src

---
+++ 11/src/main.cpp
@@ -0,0 +1,16 @@
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <concepts>
+
+/* given
+ *  
+ * */
+template<class N>
+std::vector<N> intersections(std::function<N(N)> f, std::function<N(N)>, std::pair<N, N> range) {
+
+}
+
+int main() {
+
+}
