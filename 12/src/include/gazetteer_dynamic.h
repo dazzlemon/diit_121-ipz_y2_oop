b69ce5eb56dcfe7d366de077e00172e7f361fcf0
dazzlemon <daanill13470@gmail.com>
2021-02-14 18:46:45 +0200

    almost done, need mini refactor + call more algorithms/methods + finish doc

---
+++ 12/src/include/gazetteer_dynamic.h
@@ -0,0 +1,26 @@
+/// @file gazetteer_dynamic.h
+#ifndef GAZETTEER_DYNAMIC_H
+#define GAZETTEER_DYNAMIC_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+class GazetteerDynamic {
+	std::multimap<std::string, std::string> multidict;
+public:
+	void insertInfo(const std::string& country, const std::string data) {
+		this->multidict.insert({country, data});
+	}
+
+	auto getInfo(const std::string& country) -> std::vector<std::string> {
+		auto [first, last] = multidict.equal_range(country);
+		auto info = std::vector<std::string>();
+		for (; first != last; first++) {
+			info.push_back(first->second);
+		}
+		return info;
+	} 
+};
+
+#endif
--- 12/src/main.cpp
+++ 12/src/main.cpp
@@ -1,6 +1,35 @@
 #include "gazetteer.h"
+#include "gazetteer_dynamic.h"
 #include <iostream>
 #include <vector>
+
+void testGazetteerDynamicGood() {
+	auto gazetteer = GazetteerDynamic();
+	gazetteer.insertInfo("Ukraine", "Country of Eastern Europe.");
+	gazetteer.insertInfo("Ukraine", "Second-largest country in Europe after Russia.");
+	gazetteer.insertInfo("China", "Most populous country in the world.");
+	gazetteer.insertInfo("China", "Third-largest(by area) country in the world.");
+
+	std::cout << "GazetteerDynamic tests with correct input:" << std::endl;
+	std::cout << R"(insertInfo("Ukraine", "Country of Eastern Europe.");)" << std::endl;
+	std::cout << R"(insertInfo("Ukraine", "Second-largest country in Europe after Russia.");)" << std::endl;
+	std::cout << R"(insertInfo("China", "Most populous country in the world.");)" << std::endl;
+	std::cout << R"(insertInfo("China", "Third-largest(by area) country in the world.");)" << std::endl;
+	std::cout << std::endl;
+	
+	auto countries = std::vector {
+		"Ukraine",
+		"China"
+	};
+	for (auto country : countries) {
+		auto values = gazetteer.getInfo(country);
+		std::cout << "Country: " << country << std::endl;
+		std::cout << "Values:" << std::endl;
+		for (size_t i = 0; i < values.size(); i++) {
+			std::cout << "\t" << i + 1 << ". " << values[i] << std::endl;
+		}
+	}
+}
 
 void testGazetteerGood() {
 	auto countries = std::vector {
@@ -12,7 +41,6 @@ void testGazetteerGood() {
 	for (auto country : countries) {
 		std::cout << country << ": " << Gazetteer::getInfo(country) << std::endl;
 	}
-	std::cout << std::endl;
 }
 
 void testGazetteerBad() {
@@ -29,6 +57,8 @@ void testGazetteerBad() {
 }
 
 auto main() -> int {
-	testGazetteerGood();
-	testGazetteerBad();
+	testGazetteerGood(); std::cout << std::endl;
+	testGazetteerBad(); std::cout << std::endl;
+	testGazetteerDynamicGood();
+	//testGazetteerDynamicBad();
 }	
