eb7da6fed3d474c180cb2752f361f100cb304b9f
dazzlemon <daanill13470@gmail.com>
2021-02-14 23:26:32 +0200

    2 files remaining

--- 12/src/include/gazetteer.h
+++ 12/src/include/gazetteer.h
@@ -5,10 +5,20 @@
 #include <map>
 #include <string>
 
+/**
+ * std::map wrapper to represent Gazetteer.
+ */
 class Gazetteer {
 	std::map<std::string, std::string> dict;
 public:
-	auto getInfo(const std::string& country) -> std::string {
+	/**
+	 * @brief Returns info about country.
+	 * @param[in] country Country to get info about
+	 * @return String with info about country
+	 * @details
+	 * Returns "No info about this country." if no info about this country.
+	 */
+	[[nodiscard]] auto getInfo(const std::string& country) const -> std::string {
 		auto val = this->dict.find(country);
 		if (val == this->dict.end()) {
 			return "No info about this country.";
@@ -16,12 +26,29 @@ public:
 		return val->second;
 	}
 
+	/**
+	 * @brief Inserts info about country.
+	 * @param[in] country Country to insert info about
+	 * @param[in] info Info about country
+	 * @return true in case of success(see details)
+	 * @details
+	 * Inserts info about country if there is none, returns true.
+	 * If info already exists returns false.
+	 */
 	auto insert(const std::string& country, const std::string info) -> bool {
 		bool ret = !this->dict.contains(country);
 		this->dict.insert({country, info});
 		return ret;
 	}
 
+	/**
+	 * @brief Inserts info about country.
+	 * @param[in] country Country to insert info about
+	 * @param[in] info Info about country
+	 * @return true in case of insertion, false in case of replacement
+	 * Inserts info about country if there is none, returns true.
+	 * If info already exists, replaces it returns false.
+	 */
 	auto insert_force(const std::string& country, const std::string info) -> bool {
 		return this->dict.insert_or_assign(country, info).second;
 	}
--- 12/src/include/gazetteer_multi.h
+++ 12/src/include/gazetteer_multi.h
@@ -6,13 +6,26 @@
 #include <string>
 #include <vector>
 
+/**
+ * Wrapper for std::multimap to represent Gazetteer.
+ */
 class GazetteerMulti {
 	std::multimap<std::string, std::string> multidict;
 public:
+	/**
+	 * @brief Inserts new info about country.
+	 * @param[in] country Country to insert info about
+	 * @param[in] data Info about country
+	 */
 	void insertInfo(const std::string& country, const std::string data) {
 		this->multidict.insert({country, data});
 	}
 
+	/**
+	 * @brief Returns vector of inputs about country.
+	 * @param[in] country Country to get info about
+	 * @returns Vector of input about country(may be empty)
+	 */
 	auto getInfo(const std::string& country) -> std::vector<std::string> {
 		auto [first, last] = multidict.equal_range(country);
 		auto info = std::vector<std::string>();
@@ -22,6 +35,10 @@ public:
 		return info;
 	}
 
+	/**
+	 * @brief Removes all inputs about country.
+	 * @param[in] country Country to remove inputs about
+	 */
 	void removeCountry(const std::string& country) {
 		multidict.erase(country);
 	}
