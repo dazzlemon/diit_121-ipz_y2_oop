fa5aac524c14edd5472438b1aee3d42e6678cbf3
dazzlemon <daanill13470@gmail.com>
2021-02-16 19:12:06 +0200

    formatting done

--- 13/src/java/oop13/Main.java
+++ 13/src/java/oop13/Main.java
@@ -1,19 +1,35 @@
 package oop13;
 
 public class Main {
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	
 	public static void main(String[] args) {
-		var matrix = new Matrix<Integer>(4, 3, 0);
+		var matrix = new Matrix<Integer>(5, 10, 0);
 		for (int i = 0; i < matrix.height; i++) {
 			for (int j = 0; j < matrix.width; j++) {
 				matrix.set(i, j, i * matrix.width + j);
 			}
 		}
+		
+		var indices = Solver.solve(matrix);
+		var x = indices.x;
+		var y = indices.y;
+
+		var maxLen = String.valueOf(matrix.get(x, y)).length();
 
 		for (int i = 0; i < matrix.height; i++) {
 			for (int j = 0; j < matrix.width; j++) {
-				System.out.print(matrix.get(i, j) + " ");
+				if (i == x && j == y) {
+					System.out.printf(ANSI_BLUE_BACKGROUND + "%" + maxLen + "d ", matrix.get(i, j));
+				} else if (i == x || j == y) {
+					System.out.printf(ANSI_CYAN_BACKGROUND + "%" + maxLen + "d ", matrix.get(i, j));
+				} else {
+					System.out.printf(ANSI_BLACK_BACKGROUND + "%" + maxLen + "d ", matrix.get(i, j));
+				}
 			}
-			System.out.println();
+			System.out.println(ANSI_BLACK_BACKGROUND);
 		}
 	}
 }
--- 13/src/java/oop13/Solver.java
+++ 13/src/java/oop13/Solver.java
@@ -1,7 +1,19 @@
 package oop13;
 
-public class Solver {
-	public static void solve() {
+import java.awt.Point;
 
+public class Solver {
+		public static <T extends Comparable<T>> Point solve(Matrix<T> matrix) {
+		int imax = 0;
+		int jmax = 0;
+		for (int i = 0; i < matrix.height; i++) {
+			for (int j = 0; j < matrix.width; j++) {
+				if (matrix.get(i, j).compareTo(matrix.get(imax, jmax)) > 0) {
+					imax = i;
+					jmax = j;
+				}
+			}
+		}
+		return new Point(imax, jmax);
 	}
 }
