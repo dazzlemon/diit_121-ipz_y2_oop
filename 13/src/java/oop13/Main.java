d97b3814f8bcd84775f4d86fcb88bb8829975256
dazzlemon <daanill13470@gmail.com>
2021-02-16 20:03:35 +0200

    almost done

--- 13/src/java/oop13/Main.java
+++ 13/src/java/oop13/Main.java
@@ -1,5 +1,7 @@
 package oop13;
 
+import java.awt.Point;
+
 public class Main {
 	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
@@ -16,20 +18,34 @@ public class Main {
 			}
 		}
 		
+		var zero = new Point(0, 0);
 		var indices = Solver.solve(matrix);
+		
 		var x = indices.x;
 		var y = indices.y;
+
+		print(matrix, indices, zero);
+		System.out.println();
+				
+		Solver.swapRows(matrix, 0, x);
+		Solver.swapColumns(matrix, 0, y);
+		print(matrix, indices, zero);
+	}
+
+	public static <T> void print(Matrix<T> matrix, Point pMax, Point p) {
+		var x = pMax.x;
+		var y = pMax.y;
 
 		var maxLen = String.valueOf(matrix.get(x, y)).length();
 
 		for (int i = 0; i < matrix.height; i++) {
 			for (int j = 0; j < matrix.width; j++) {
-				var color = ((i == x || j == y) && (i == 0 || j == 0)) ? ANSI_GREEN_BACKGROUND
-				          :  (i == x && j == y)                        ? ANSI_CYAN_BACKGROUND
-				          :  (i == x || j == y)                        ? ANSI_LIGHT_CYAN_BACKGROUND
-				          :  (i == 0 && j == 0)                        ? ANSI_YELLOW_BACKGROUND
-				          :  (i == 0 || j == 0)                        ? ANSI_LIGHT_YELLOW_BACKGROUND
-				                                                       : ANSI_RESET;
+				var color = ((i == x || j == y) && (i == p.x || j == p.y)) ? ANSI_GREEN_BACKGROUND
+				          :  (i == x && j == y)                            ? ANSI_CYAN_BACKGROUND
+				          :  (i == x || j == y)                            ? ANSI_LIGHT_CYAN_BACKGROUND
+				          :  (i == p.x && j == p.y)                        ? ANSI_YELLOW_BACKGROUND
+				          :  (i == p.x || j == p.y)                        ? ANSI_LIGHT_YELLOW_BACKGROUND
+				                                                           : ANSI_RESET;
 				System.out.printf(color + "%" + maxLen + "d ", matrix.get(i, j));
 			}
 			System.out.println(ANSI_RESET);
--- 13/src/java/oop13/Solver.java
+++ 13/src/java/oop13/Solver.java
@@ -3,7 +3,7 @@ package oop13;
 import java.awt.Point;
 
 public class Solver {
-		public static <T extends Comparable<T>> Point solve(Matrix<T> matrix) {
+	public static <T extends Comparable<T>> Point solve(Matrix<T> matrix) {
 		int imax = 0;
 		int jmax = 0;
 		for (int i = 0; i < matrix.height; i++) {
@@ -15,5 +15,21 @@ public class Solver {
 			}
 		}
 		return new Point(imax, jmax);
+	}
+	
+	public static <T> void swapRows(Matrix<T> matrix, int i1, int i2) {
+		for (int j = 0; j < matrix.width; j++) {
+			var tmp = matrix.get(i1, j);
+			matrix.set(i1, j, matrix.get(i2, j));
+			matrix.set(i2, j, tmp);
+		}
+	}
+	
+	public static <T> void swapColumns(Matrix<T> matrix, int j1, int j2) {
+		for (int i = 0; i < matrix.height; i++) {
+			var tmp = matrix.get(i, j1);
+			matrix.set(i, j1, matrix.get(i, j2));
+			matrix.set(i, j2, tmp);
+		}
 	}
 }
