eddec9dc9a4c395f8ae08fa3c50fa0af12ed6da0
dazzlemon <daanill13470@gmail.com>
2021-02-16 19:37:17 +0200

    colorcode

--- 13/src/java/oop13/Main.java
+++ 13/src/java/oop13/Main.java
@@ -1,9 +1,12 @@
 package oop13;
 
 public class Main {
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_LIGHT_CYAN_BACKGROUND = "\u001B[106m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_LIGHT_YELLOW_BACKGROUND = "\u001B[103m";
+	public static final String ANSI_RESET = "\u001B[0m";
 	
 	public static void main(String[] args) {
 		var matrix = new Matrix<Integer>(5, 10, 0);
@@ -21,12 +24,15 @@ public class Main {
 
 		for (int i = 0; i < matrix.height; i++) {
 			for (int j = 0; j < matrix.width; j++) {
-				var color = (i == x && j == y) ? ANSI_BLUE_BACKGROUND
-				          : (i == x || j == y) ? ANSI_CYAN_BACKGROUND
-				                               : ANSI_BLACK_BACKGROUND;
+				var color = ((i == x || j == y) && (i == 0 || j == 0)) ? ANSI_GREEN_BACKGROUND
+					        : (i == x && j == y) ? ANSI_CYAN_BACKGROUND
+				          : (i == x || j == y) ? ANSI_LIGHT_CYAN_BACKGROUND
+				          : (i == 0 && j == 0) ? ANSI_YELLOW_BACKGROUND
+									: (i == 0 || j == 0) ? ANSI_LIGHT_YELLOW_BACKGROUND
+				                               : ANSI_RESET;
 				System.out.printf(color + "%" + maxLen + "d ", matrix.get(i, j));
 			}
-			System.out.println(ANSI_BLACK_BACKGROUND);
+			System.out.println(ANSI_RESET);
 		}
 	}
 }
