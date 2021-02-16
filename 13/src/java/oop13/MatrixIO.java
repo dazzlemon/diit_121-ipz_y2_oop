f1eb5669869c798bb42f2b297da25eff2a1ef54b
dazzlemon <daanill13470@gmail.com>
2021-02-16 20:10:29 +0200

    MatrixIO

---
+++ 13/src/java/oop13/MatrixIO.java
@@ -0,0 +1,40 @@
+package oop13;
+
+import java.awt.Point;
+
+public class MatrixIO {
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_LIGHT_CYAN_BACKGROUND = "\u001B[106m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_LIGHT_YELLOW_BACKGROUND = "\u001B[103m";
+	public static final String ANSI_RESET = "\u001B[0m";
+
+	public static void read(Matrix<Integer> matrix) {
+		for (int i = 0; i < matrix.height; i++) {
+			for (int j = 0; j < matrix.width; j++) {
+				matrix.set(i, j, i * matrix.width + j);
+			}
+		}
+	}
+
+	public static <T> void print(Matrix<T> matrix, Point pMax, Point p) {
+		var x = pMax.x;
+		var y = pMax.y;
+
+		var maxLen = String.valueOf(matrix.get(x, y)).length();
+
+		for (int i = 0; i < matrix.height; i++) {
+			for (int j = 0; j < matrix.width; j++) {
+				var color = ((i == x || j == y) && (i == p.x || j == p.y)) ? ANSI_GREEN_BACKGROUND
+				          :  (i == x && j == y)                            ? ANSI_CYAN_BACKGROUND
+				          :  (i == x || j == y)                            ? ANSI_LIGHT_CYAN_BACKGROUND
+				          :  (i == p.x && j == p.y)                        ? ANSI_YELLOW_BACKGROUND
+				          :  (i == p.x || j == p.y)                        ? ANSI_LIGHT_YELLOW_BACKGROUND
+				                                                           : ANSI_RESET;
+				System.out.printf(color + "%" + maxLen + "d ", matrix.get(i, j));
+			}
+			System.out.println(ANSI_RESET);
+		}
+	}
+}
