b6bbffe590e6f7d2c538697ea81c775414d26167
dazzlemon <daanill13470@gmail.com>
2021-02-17 10:27:15 +0200

    code seems done

--- 13/src/java/oop13/Main.java
+++ 13/src/java/oop13/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
 		var in = new Scanner(System.in);
 
 		System.out.print("Input matrix' height: ");
@@ -14,8 +13,17 @@ public class Main {
 		System.out.print("Input matrix' width: ");
 		var w = in.nextInt();
 
+		var in2 = new Scanner(System.in);
+		System.out.print("Would you like to fill matrix yourself(Y - yourself, any other input - random): ");
+		var choice = in2.next();
+
 		var matrix = new Matrix<Integer>(h, w, 0);
-		MatrixIO.read(matrix);
+		
+		if (choice.compareTo("Y") == 0) {
+			MatrixIO.read(matrix);
+		} else {
+			MatrixIO.randFill(matrix);
+		}
 
 		var zero = new Point(0, 0);
 		var indices = Solver.solve(matrix);
