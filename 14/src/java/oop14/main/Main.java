600cf85368f941d3d3e526735e0f18061b1412a5
dazzlemon <daanill13470@gmail.com>
2021-02-22 10:09:23 +0200

    javax seems to work

---
+++ 14/src/java/oop14/main/Main.java
@@ -0,0 +1,12 @@
+package oop14.main;
+
+import javax.swing.JFrame;
+
+public class Main {
+  public static void main(String[] args) {
+    var frame = new JFrame();
+    frame.setSize(800, 600);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  }
+}
