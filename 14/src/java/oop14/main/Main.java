4dbbb367cd29e641c6b21024b593f43e09f045c4
dazzlemon <daanill13470@gmail.com>
2021-02-23 12:20:40 +0200

    netbeans proj 14

--- 14/src/java/oop14/main/Main.java
+++ 14/src/java/oop14/main/Main.java
@@ -1,12 +1,34 @@
-package oop14.main;
+import javax.swing.*;
+import java.awt.*;
 
-import javax.swing.JFrame;
 
 public class Main {
   public static void main(String[] args) {
-    var frame = new JFrame();
-    frame.setSize(800, 600);
+    var frame = new MyFrame();
+
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
+  }
+}
+
+
+class MyFrame extends JFrame {
+  public MyFrame() {
+    setSize(WIDTH, HEIGHT);
+    var panel = new PaintPanel();
+    add(panel);
+  }
+
+  public static final int WIDTH = 800;
+  public static final int HEIGHT = 600;
+}
+
+
+class PaintPanel extends JPanel {
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    g.drawString("test", 10, 20);
+    g.drawRect(10, 40, 300, 100);
   }
 }
