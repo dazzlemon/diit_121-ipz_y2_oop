7397fe3cb815d4b9e414e6c234944355b5108f55
dazzlemon <daanill13470@gmail.com>
2021-02-26 16:38:57 +0200

    copied oop14 to oop15

---
+++ 15/netbeans/oop15/src/main/java/com/dazzlemon/oop15/Matrix.java
@@ -0,0 +1,33 @@
+package com.dazzlemon.oop15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Matrix <T> {
+    public final int width;
+    public final int height;
+    private List<List<T>> matrix;
+    
+    
+    public Matrix(int height, int width, T filler) {
+	this.height = height;
+	this.width  = width;
+		
+	this.matrix = new ArrayList<>(height);
+	for (int i = 0; i < height; i++) {
+            this.matrix.add(i, new ArrayList<>(width));
+            for (int j = 0; j < width; j++) {
+		this.matrix.get(i).add(filler);
+            }
+	}
+    }
+
+    
+    public void set(int i, int j, T val) {
+	this.matrix.get(i).set(j, val);
+    }
+
+    public T get(int i, int j) {
+	return this.matrix.get(i).get(j);
+    }
+}
